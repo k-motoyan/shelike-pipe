dbb8cdee135425dc6dd168880d08ec395a884f0e
k-motoyan <k.motoyan888@gmail.com>
2016-04-04 21:36:47 +0900

    Any tweaks.

--- spec/shelike/pipe/version_spec.rb
+++ spec/shelike/pipe/version_spec.rb
@@ -1,5 +1,7 @@
+require 'shelike/pipe/version'
+
 RSpec.describe Shelike::Pipe::VERSION do
   describe 'Version' do
-    it { expect(Shelike::Pipe::VERSION).to eq '0.1.0' }
+    it { expect(Shelike::Pipe::VERSION).to eq '0.1.1' }
   end
 end
